f8e939badd3c3eb24b262f81e38bdbbbaef52266
“[Krishnakant-pal]” <krishnakant.pal127@gmail.com>
2025-04-08 21:01:33 +0530

    Add solution

--- Number_Theory/count-squares.py
+++ Number_Theory/count-squares.py
@@ -5,3 +5,31 @@ class Solution:
         if sqrt*sqrt == N:
             return int(N**0.5) -1
         return int(N**0.5)
+
+# Alternate approach using Binary search
+class Solution:
+    def countSquares(self, n):
+        if n <=1:
+            return 0
+        low = 1
+        high = n-1
+        res = 0
+        
+        while low <=high:
+            mid = (low +high)//2
+            if mid * mid < n:
+                res = mid
+                low = mid +1
+            else:
+                high = mid -1
+        return res
+    
+# Simple approach
+class Solution:
+    def countSquares(self, n):
+        # code here 
+        sqrt =  int((n**0.5 ))
+        if sqrt *sqrt == n:
+            return sqrt -1
+        else :
+            return sqrt 
